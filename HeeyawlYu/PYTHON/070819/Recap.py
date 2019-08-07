6fe047eaf1acd5b19d15c8c2499afab367c6d3e0
Heeyawl Yu <heeyawl@gmail.com>
2019-08-07 16:58:35 +0900

    다혜님 술 그만 마셔요

---
+++ HeeyawlYu/PYTHON/070819/Recap.py
@@ -0,0 +1,31 @@
+scores = [1, 2, 3, 4, 5, 6]
+
+
+# total_scores = scores[0] + scores[1]
+
+# for i in range(0, len(scores)):
+#     total_scores = 0
+#     total_scores += scores[i]
+
+def f(x):
+    return x[0] + x[1]
+
+
+#Red - Green - RE
+
+print("Total_scores:", f([1, 2]))
+
+
+def double(n):
+    return n * 2
+
+
+print(double(3))
+
+
+scores[0] + scores[1]
+
+f(x) = 2x + 1
+
+f(2)
+
